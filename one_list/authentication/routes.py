8c5366a9efb65d5249607e6648ff9de6472a6960
Owen Carvill <owen.carvill@gmail.com>
2021-09-27 20:23:11 -0500

    More work on flask

---
+++ one_list/authentication/routes.py
@@ -0,0 +1,33 @@
+from flask import Blueprint, render_template, request, redirect,url_for,flash
+# from one_list.models import User,db
+from one_list.forms import UserLoginForm
+
+
+auth = Blueprint('auth', __name__, template_folder = 'auth_templates')
+
+@auth.route('/signup', methods = ['GET', 'POST'])
+def signup():
+    form = UserLoginForm()
+
+    try:
+        if request.method == 'POST' and form.validate_on_submit():
+            email = form.email.data
+            password = form.password.data
+            print(email,password)
+
+            user = User(email, password = password)
+
+            db.session.add(user)
+            db.session.commit()
+
+            flash(f'You have successfully created a user account {email}', 'user-created')
+
+            return redirect(url_for('site.home'))
+
+    except:
+        raise Exception('Invalid Form Data: Please Check your form')
+    return render_template('signup.html', form=form)
+
+@auth.route('/signin', methods = ['GET', 'POST'])
+def signin():
+    return render_template('signin.html')
